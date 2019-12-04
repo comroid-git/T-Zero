88e94c309ce47c7f231dab55ee03186aa64c54f7
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2019-12-04 14:33:34 +0100

    Fix Bot

--- src/main/java/de/comroid/util/files/FileProvider.java
+++ src/main/java/de/comroid/util/files/FileProvider.java
@@ -4,31 +4,36 @@ import java.io.File;
 import java.io.IOException;
 import java.util.stream.IntStream;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static java.io.File.separator;
 import static java.io.File.separatorChar;
 
 public class FileProvider {
+    public final static Logger logger = LogManager.getLogger();
     private final static String PREFIX = "/app/data/";
 
     public static File getFile(String subPath) {
         final String path = (PREFIX + subPath).replace('/', separatorChar);
-        System.out.printf("Acquiring File [ %s ]\n", path);
+        logger.printf(Level.INFO, "Acquiring File [ %s ]", path);
 
         createDirs(path);
 
         final File file = new File(path);
 
         if (!file.exists()) {
-            System.out.printf("File [ %s ] does not exist. Trying to create it...\n", path);
+            logger.printf(Level.WARN, "GET %s FAIL: File does not exist. Trying to create it...", path);
 
             try {
                 if (!file.createNewFile()) {
-                    System.out.printf(" FAIL: Could not create File [ %s ] for unknown reason. Exiting.\n", path);
+                    logger.printf(Level.ERROR, "CREATE %s FAIL: Could not create File for unknown reason. Exiting.", path);
                     System.exit(1);
                     return null; // lol
-                } else System.out.print(" OK!\n");
+                } else logger.printf(Level.INFO, "CREATE %s OK!", path);
             } catch (IOException e) {
-                System.out.printf(" FAIL: An [ %s ] occurred creating File [ %s ]. Exiting.\n", e.getClass().getSimpleName(), path);
+                logger.printf(Level.ERROR, "CREATE %s FAIL: An [ %s ] occurred creating File. Exiting.", path, e.getClass().getSimpleName());
                 e.printStackTrace(System.out);
                 System.exit(1);
                 return null; // lol
@@ -39,16 +44,14 @@ public class FileProvider {
     }
 
     private static void createDirs(final String forPath) {
-        System.out.printf("Checking directories for file [ %s ]...", forPath);
+        logger.printf(Level.INFO, "Checking directories for file [ %s ]...", forPath);
 
         final String[] paths = forPath.split(separator);
 
         if (paths.length <= 1) {
-            System.out.printf(" OK! [ %d ]\n", paths.length);
+            logger.printf(Level.INFO, "CREATE PATHS %s OK! [ %d ]", forPath, paths.length - 1);
             return;
         }
-
-        int[] printed = new int[]{0};
 
         IntStream.range(0, paths.length)
                 .mapToObj(value -> {
@@ -64,21 +67,14 @@ public class FileProvider {
                     if (file.exists() && file.isDirectory())
                         return;
 
-                    printed[0]++;
-                    System.out.printf(" FAIL\nDirectory [ %s ] does not exist, trying to create it...", path);
+                    logger.printf(Level.ERROR, "GET PATH %s FAIL: Directory does not exist, trying to create it...", path);
 
-                    if (file.mkdir()) {
-                        printed[0]++;
-                        System.out.printf(" OK!\nCreated directory [ %s ] for file [ %s ]\n", path, forPath);
-                    } else {
-                        printed[0]++;
-                        System.out.printf(" FAIL\nCould not create directory [ %s ] for file [ %s ]! Exiting.\n", path, forPath);
+                    if (file.mkdir())
+                        logger.printf(Level.INFO, "CREATE PATH %s OK!", path);
+                    else {
+                        logger.printf(Level.ERROR, "CREATE PATH %s FAIL: Could not create directory for unknown reason.", path, forPath);
                         System.exit(1);
                     }
                 });
-
-        if (printed[0] == 0)
-            System.out.print(" OK!\n");
-        else System.out.println();
     }
 }
